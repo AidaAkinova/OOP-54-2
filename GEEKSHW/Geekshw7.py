68f1bea94053134dea2e8d316fd2cbad9bd1e263
AidaAkinova <akinovaaida.89231196656@gmail.com>
2025-05-30 00:41:12 +0600

    Doing HW7

--- GEEKSHW/Geekshw7.py
+++ GEEKSHW/Geekshw7.py
@@ -1,10 +1,9 @@
-
 import sqlite3
 
 
 def get_user_by_id(user_id):
     """
-    Функция для поиска пользователя по ID в базе данных.
+    Функция для поиска пользователя по ID в базе данных SQLite
 
     Args:
         user_id (int): ID пользователя для поиска
@@ -13,10 +12,11 @@ def get_user_by_id(user_id):
         dict or None: Возвращает данные пользователя в виде словаря, если найден,
                      или None, если пользователь не найден
     """
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-
+    conn = None
     try:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
         cursor.execute("SELECT * FROM users WHERE id = ?", (user_id,))
         user_data = cursor.fetchone()
 
@@ -32,9 +32,26 @@ def get_user_by_id(user_id):
         return None
 
     finally:
-        conn.close()
+        if conn:
+            conn.close()
 
 
 if __name__ == "__main__":
-    user = get_user_by_id(1)
-    print(user)
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            email TEXT
+        )
+    """)
+
+    cursor.execute("INSERT OR IGNORE INTO users VALUES (1, 'Иван Иванов', 'ivan@example.com')")
+    cursor.execute("INSERT OR IGNORE INTO users VALUES (2, 'Петр Петров', 'petr@example.com')")
+    conn.commit()
+    conn.close()
+
+
+    print(get_user_by_id(1))
+    print(get_user_by_id(3))
